82083f8263f813dc2dd5d203702e65fc4f58ad1c
= <=>
2026-03-02 16:43:44 +0530

    Added ds2.java file

---
+++ ds2.java
@@ -0,0 +1,11 @@
+{
+
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
